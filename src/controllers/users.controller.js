71b829a4061ac1b936f5809fec8b16e5c4049910
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-11-23 17:15:27 -0300

    feat: create jwt validation and sign-up route

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,8 +1,24 @@
 import { usersCollection } from '../database/db.js';
-import { v4 as uuidV4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export async function postUserSignUp(req, res) {
+  const { email, cpf, name, password } = req.body;
   try {
+    const hashPassword = bcrypt.hashSync(password, 12);
+    const createdUser = await usersCollection.insertOne({
+      email,
+      name,
+      cpf,
+      password: hashPassword,
+    });
+    const token = jwt.sign({ id: createdUser._id }, process.env.SECRET_JWT, {
+      expiresIn: 86400,
+    });
+    res.status(201).send({ token });
   } catch (err) {
     res.sendStatus(500);
   }
---
+++ src/middlewares/jwtValidation.middleware.js
@@ -0,0 +1,44 @@
+import { ObjectId } from 'mongodb';
+import { usersCollection } from '../database/db';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default async function jwtValidation(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: 'The token was not informed!' });
+  }
+
+  const parts = authHeader.split(' ');
+
+  if (parts.length !== 2) {
+    return res.status(401).send({ message: 'Invalid token!' });
+  }
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).send({ message: 'Malformatted Token!' });
+  }
+
+  jwt.verify(token, process.env.SECRET_JWT, async (err, decoded) => {
+    if (err) {
+      console.error(err);
+      return res.status(401).send({ message: 'Invalid token!' });
+    }
+
+    const user = await usersCollection.findOne({
+      _id: new ObjectId(decoded.id),
+    });
+
+    if (!user || !user._id) {
+      return res.status(401).send({ message: 'Invalid token!' });
+    }
+
+    res.locals.userId = user._id;
+
+    return next();
+  });
+}
