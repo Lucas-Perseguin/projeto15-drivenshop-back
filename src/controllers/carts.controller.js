8879e2891541ffb484a2adeea79cf3636fa8fc13
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-11-24 01:51:35 -0300

    feat: add product to user cart

---
+++ src/controllers/carts.controller.js
@@ -0,0 +1,32 @@
+import { cartscollections } from '../database/db.js';
+
+export async function postProductToCart(req, res) {
+  const { productId } = req.query;
+  if (!productId) {
+    return res.sendStatus(400);
+  }
+
+  const { amount } = req.body;
+  if (!amount) {
+    return res.sendStatus(400);
+  }
+
+  const { userId } = res.locals;
+
+  try {
+    const productFoud = await cartscollections.findOne({ productId, userId });
+    if (!productFoud) {
+      await cartscollections.insertOne({ userId, productId, amount });
+      return res.sendStatus(201);
+    } else {
+      const amountToSet = amount + productFoud.amount;
+      await cartscollections.updateOne(
+        { userId, productId },
+        { $set: { amount: amountToSet } }
+      );
+      return res.sendStatus(200);
+    }
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+}
