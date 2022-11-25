f26e60456697dfe80548d521b55094fe9f26a034
Lucas F. Silva <lucasdotfs@gmail.com>
2022-11-25 17:27:48 -0300

    feat: add productByType route

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,5 +1,5 @@
-import { ObjectId } from 'mongodb';
-import { productsCollection } from '../database/db.js';
+import { ObjectId } from "mongodb";
+import { productsCollection } from "../database/db.js";
 
 export async function getProducts(req, res) {
   try {
@@ -25,6 +25,38 @@ export async function getProductById(req, res) {
     }
 
     res.status(200).send(productFound);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+}
+
+export async function getProductsByType(req, res) {
+  const { productType } = req.params;
+  if (!productType) return res.sendStatus(400);
+  try {
+    const products = await productsCollection
+      .find({ type: productType })
+      .toArray();
+
+    if (!products) return res.sendStatus(404);
+
+    res.sendStatus(200).send(products);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+}
+
+export async function getProductsByTypeOnSale(req, res) {
+  const { productType } = req.params;
+
+  if (!productType) return res.sendStatus(400);
+
+  try {
+    const products = await productsCollection
+      .find({ type: productType, sale: true })
+      .toArray();
+    if (!products) return res.sendStatus(404);
+    res.sendStatus(200).send(products);
   } catch (err) {
     res.sendStatus(500);
   }
