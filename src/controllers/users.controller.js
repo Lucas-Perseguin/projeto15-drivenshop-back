7d75695b8b4158ded5b14ff6d9a365bd57037a49
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-11-27 18:16:25 -0300

    refact: res data

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,7 +1,7 @@
-import { usersCollection } from "../database/db.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import { usersCollection } from '../database/db.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -18,7 +18,7 @@ export async function postUserSignUp(req, res) {
     const token = jwt.sign({ id: createdUser._id }, process.env.SECRET_JWT, {
       expiresIn: 86400,
     });
-    res.status(201).send({ token });
+    res.status(201).send(token);
   } catch (err) {
     res.sendStatus(500);
   }
